ef030dce4e3982c7a56b54604f61682db7975ee1
SahanBuddhika <sahan.bu.1994@gmail.com>
2019-05-04 22:11:06 +0530

    eddit main file

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const ejs = require('ejs');
 const Nexmo = require('nexmo');
 const socketio = require('socket.io');
 
+//init Nexmo 
+const nexmo = new Nexmo({
+    apiKey: '2b80c827',
+    apiSecret: 'XB8fecbsNdg7h0Pu'
+}, {debug:true});
+
 //Init app
 const app = express();
 
@@ -14,7 +20,7 @@ app.engine('html', ejs.renderFile);
 //public folder setup
 app.use(express.static(__dirname + '/public'));
 
-//body parser middleware
+//body parser middleware 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -25,8 +31,22 @@ app.get('/' , (req , res) =>{
 
 //catch form submit
  app.post('/',(req , res)=>{
-     res.send(req.body);
-     console.log(req.body);
+    //  res.send(req.body);
+    //  console.log(req.body);
+
+    const number = req.body.number;
+    const text = req.body.text;
+
+    nexmo.message.sendSms(
+        '94766924803' , number , text , { type: 'unicode'},
+        (err , responseData)=>{
+            if(err){
+                console.log(err);
+            }else{
+                console.dir(responseData);
+            }
+        }
+    )
  })
 
 //define port 
